147bbd7a757270d99e7bc251a55d7ea03497eb65
akshatsharma911538-dev <akshatsharma911538@gmail.com>
2026-08-21 18:56:28 +0530

    Initial commit

---
+++ hello.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int day; //1- Mon,2-Tue,3-Wed,4-Thur,5-Fri,6-Sat,7-Sun
+    printf("enter your day:");
+    scanf("%d",&day);
+    
+    switch (day){
+        case 1:
+        printf("Monday \n");
+        break;
+        case 2:
+        printf("Tuesday \n");
+        break;
+        case 3:
+        printf("Wednesday \n");
+        break;
+        case 4:
+        printf("Thursday \n");
+        break;
+        case 5:
+        printf("Friday \n");
+        break;
+        case 6:
+        printf("Saturday \n");
+        break;
+        case 7:
+        printf("Sunday \n");
+        break;
+        default:printf("not a valid day! \n");
+    }
+    
